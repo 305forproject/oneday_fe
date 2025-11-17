233f30fc5c1d68610db1fcf2ba05451f26fa63ae
kyeonghwi <kyeonghwi@gmail.com>
2025-11-17 17:09:20 +0900

    setup

---
+++ src/Router.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// 레이아웃
+import Layout from "./features/layout/Layout";
+
+// 페이지 컴포넌트
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import ClassDetail from "./pages/ClassDetail";
+import MyPage from "./pages/MyPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 헤더/푸터가 필요한 페이지 */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/classes/:classId" element={<ClassDetail />} />
+          <Route path="/my" element={<MyPage />} />
+        </Route>
+
+        {/* 헤더/푸터가 없는 페이지  */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
